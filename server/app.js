a802b821975510212cbd3c5c0b3f1d750db794d8
vishnu teja vasam <vishnutejavasam.com>
2026-06-28 19:15:55 +0530

    chore(server): initialize express server with basic middleware

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+// Basic route for testing
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Lost and Found Portal API' });
+});
+
+module.exports = app;
