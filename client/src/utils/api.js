c0a54905e9c4c724a03c4cd908cd913bec335bf4
vishnu teja vasam <vishnutejavasam.com>
2026-06-29 13:01:10 +0530

    fix(client): add withCredentials to axios for cross-origin cookies

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: "https://lost-and-found-portal-ksnh.onrender.com/api",
+  withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
